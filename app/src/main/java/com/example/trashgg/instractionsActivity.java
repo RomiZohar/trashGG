753d2337077c7eb524e104806ff6892ab6f42d56
Lenovo <romizohar@gmail.com>
2024-03-16 14:57:16 +0200

    check3

--- app/src/main/java/com/example/trashgg/instractionsActivity.java
+++ app/src/main/java/com/example/trashgg/instractionsActivity.java
@@ -1,6 +1,9 @@
 package com.example.trashgg;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class instractionsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button backHomeButton = findViewById(R.id.backHomeButton);
+
+        backHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backHomeIntent = new Intent(instractionsActivity.this , MainActivity.class);
+                startActivity(backHomeIntent);
+            }
         });
     }
 }
